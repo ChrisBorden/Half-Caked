7d852653daf703794173bfbafbc9589a81d82d9d
David Weinberg <dweinberg@ufl.edu>
2012-04-05 18:49:59 -0400

    Centered title: More UI changes to follow

--- Half-Caked/Half-Caked/Screens/MenuScreen.cs
+++ Half-Caked/Half-Caked/Screens/MenuScreen.cs
@@ -263,7 +263,7 @@ namespace Half_Caked
             }
 
             // Draw the menu title.
-            Vector2 titlePosition = new Vector2(426, 80);
+            Vector2 titlePosition = new Vector2(ScreenManager.GraphicsDevice.Viewport.Width/2, 80);
             Vector2 titleOrigin = font.MeasureString(menuTitle) / 2;
             Color titleColor = new Color(192, 192, 192, TransitionAlpha);
             float titleScale = 1.25f;
